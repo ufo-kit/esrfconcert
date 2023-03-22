4501dc87db294143344844c6ea82f52556673d3d
Tomas Farago <sensej007@email.cz>
2023-03-22 13:49:59 +0100

    Implement BLISS shutter

--- esrfconcert/devices/shutters/bliss.py
+++ esrfconcert/devices/shutters/bliss.py
@@ -1,27 +1,20 @@
 """Bliss shutter."""
-from concert.base import transition
 from concert.devices.shutters import base
 
 
 class Shutter(base.Shutter):
 
-    """A dummy shutter that can be opened and closed."""
+    """A Bliss shutter implementation."""
 
-    def __init__(self, name):
-        super(Shutter, self).__init__()
-        self._name = name
+    async def __ainit__(self, device):
+        await super().__ainit__()
+        self._device = device
 
     async def _open(self):
-        self.open()
+        self._device.open()
 
     async def _close(self):
-        self.close()
+        self._device.close()
 
     async def _get_state(self):
-        state = self.state_string()
-        # .state_string() returns either "OPEN", "CLOSED", or "UNKNOWN"
-        
-        if state == "UNKNOWN":
-            print('Shutter {} is in unknown state'.format(self.name))
-        else:
-            print('Shutter {} is {}'.format(self.name, state))
+        return self._device.state.name.lower()
